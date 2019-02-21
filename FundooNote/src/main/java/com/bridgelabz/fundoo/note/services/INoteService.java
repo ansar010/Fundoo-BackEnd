b458bd95e66616f8a393d487ac597fa7571a6009
ansar <ansar@ansar>
2019-02-22 02:43:09 +0530

    22feb

--- FundooNote/src/main/java/com/bridgelabz/fundoo/note/services/INoteService.java
+++ FundooNote/src/main/java/com/bridgelabz/fundoo/note/services/INoteService.java
@@ -1,9 +1,11 @@
 package com.bridgelabz.fundoo.note.services;
 
+import java.io.UnsupportedEncodingException;
+
 import com.bridgelabz.fundoo.note.dto.NoteDTO;
 import com.bridgelabz.fundoo.note.exception.NoteException;
 
 public interface INoteService {
 
-	public boolean addNote(NoteDTO note , String token) throws NoteException;
+	public boolean addNote(NoteDTO note , String token) throws NoteException, IllegalArgumentException, UnsupportedEncodingException;
 }
--- FundooNote/src/main/java/com/bridgelabz/fundoo/note/services/NoteServiceImp.java
+++ FundooNote/src/main/java/com/bridgelabz/fundoo/note/services/NoteServiceImp.java
@@ -1,15 +1,29 @@
 package com.bridgelabz.fundoo.note.services;
 
+import java.io.UnsupportedEncodingException;
+
+import org.modelmapper.ModelMapper;
+
+import com.bridgelabz.fundoo.note.dao.INoteRepository;
 import com.bridgelabz.fundoo.note.dto.NoteDTO;
 import com.bridgelabz.fundoo.note.exception.NoteException;
+import com.bridgelabz.fundoo.note.model.Note;
 import com.bridgelabz.fundoo.utility.UserToken;
 
 public class NoteServiceImp implements INoteService{
 
+	INoteRepository noteRepository;
+	
+	ModelMapper modelMapper;
+	
 	@Override
-	public boolean addNote(NoteDTO note, String token) throws NoteException
+	public boolean addNote(NoteDTO noteDTO, String token) throws NoteException, IllegalArgumentException, UnsupportedEncodingException
 	{
-		//long userId = UserToken.tokenVerify(token);
+		long userId = UserToken.tokenVerify(token);
+		Note note = modelMapper.map(noteDTO, Note.class);
+		
+		note.setUser(note.getUser());
+		noteRepository.save(note);
 		return false;
 	}
 
