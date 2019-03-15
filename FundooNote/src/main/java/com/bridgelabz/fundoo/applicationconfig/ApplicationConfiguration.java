4062b6789556b23697abc7af4c7e52b59f426e35
ansar010 <ansaruddeen030@gmail.com>
2019-03-15 19:23:31 +0530

    15march

--- FundooNote/src/main/java/com/bridgelabz/fundoo/applicationconfig/ApplicationConfiguration.java
+++ FundooNote/src/main/java/com/bridgelabz/fundoo/applicationconfig/ApplicationConfiguration.java
@@ -1,47 +1,47 @@
-//package com.bridgelabz.fundoo.applicationconfig;
-//
-//import org.apache.catalina.connector.Response;
-//import org.modelmapper.ModelMapper;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-//import org.springframework.security.crypto.password.PasswordEncoder;
-//
-//@Configuration
-//public class ApplicationConfiguration 
-//{
+package com.bridgelabz.fundoo.applicationconfig;
+
+import org.apache.catalina.connector.Response;
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class ApplicationConfiguration 
+{
+	@Bean
+	public PasswordEncoder passwordEncoder()
+	{
+		return new BCryptPasswordEncoder();
+	}
+
+	//	@Bean
+	//	public static PropertySourcesPlaceholderConfigurer Property()
+	//	{
+	//		return new PropertySourcesPlaceholderConfigurer();
+	//	}
+
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
+	}
+
+	@Bean
+	public Response getResponse()
+	{
+		return new Response();
+	}
+	
+////	
 //	@Bean
-//	public PasswordEncoder passwordEncoder()
+//	public UserToken getToken()
 //	{
-//		return new BCryptPasswordEncoder();
+//		return new UserToken();
 //	}
-//
-//	//	@Bean
-//	//	public static PropertySourcesPlaceholderConfigurer Property()
-//	//	{
-//	//		return new PropertySourcesPlaceholderConfigurer();
-//	//	}
-//
 //	@Bean
-//	public ModelMapper modelMapper() {
-//		return new ModelMapper();
-//	}
-//
-//	@Bean
-//	public Response getResponse()
+//	public ResponseToken getResponseToken()
 //	{
-//		return new Response();
+//		return new ResponseToken();
 //	}
-//	
-//////	
-////	@Bean
-////	public UserToken getToken()
-////	{
-////		return new UserToken();
-////	}
-////	@Bean
-////	public ResponseToken getResponseToken()
-////	{
-////		return new ResponseToken();
-////	}
-//}
+}
